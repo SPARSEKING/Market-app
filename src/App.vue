5a76e890e80ce029ea8acb18edb75cbcafc4a955
Kirill Gavrilchik <gavrilchikkirill6@gmail.com>
2021-02-12 17:39:50 +0300

    feat: added routing

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <header>
+    <div class="navbar">
+      <div class="container">
+        <div class="navbar-content">
+          <div class="logo">MARKET-APP</div>
+          <ul class="navbar-list">
+            <li class="navbar-item" v-for="link in links" :key="link.title">
+              <router-link class="navbar-link" :to="link.url">{{
+                link.title
+              }}</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
   <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from "vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      links: [
+        {
+          title: "Home",
+          url: "/"
+        },
+        {
+          title: "About",
+          url: "/about"
+        }
+      ]
     }
   }
+});
+</script>
+
+<style scoped>
+.router-link-exact-active {
+  color: #5247e7;
 }
 </style>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1>Home page</h1>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 });
 </script>
+
